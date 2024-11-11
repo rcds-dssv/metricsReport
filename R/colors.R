b6dc08a664e831a974d6e9c8e6daa361ef88ac9b
John Lee <swannyy.stat@gmail.com>
2024-11-11 16:02:01 -0600

    add color functions for nw

---
+++ R/colors.R
@@ -0,0 +1,35 @@
+nw_purples <- function() {
+  c(
+    nw_purple = rgb(078, 042, 132, maxColorValue = 255),
+    purple_90 = rgb(91, 59, 140, maxColorValue = 255),
+    purple_80 = rgb(104, 76, 150, maxColorValue = 255),
+    purple_70 = rgb(118, 93, 160, maxColorValue = 255),
+    purple_60 = rgb(131, 110, 170, maxColorValue = 255),
+    purple_50 = rgb(142, 128, 182, maxColorValue = 255),
+    purple_40 = rgb(164, 149, 195, maxColorValue = 255),
+    purple_30 = rgb(182, 172, 209, maxColorValue = 255),
+    purple_20 = rgb(204, 196, 223, maxColorValue = 255),
+    purple_10 = rgb(228, 224, 238, maxColorValue = 255),
+    purple_160 = rgb(29,2,53, maxColorValue = 255),
+    purple_150 = rgb(38, 8, 65, maxColorValue = 255),
+    purple_140 = rgb(48, 16, 78, maxColorValue = 255),
+    purple_130 = rgb(56, 23, 90, maxColorValue = 255),
+    purple_120 = rgb(64, 31, 104, maxColorValue = 255),
+    purple_110 = rgb(72, 36, 118, maxColorValue = 255),
+  )
+}
+
+nw_greys <- function() {
+  c(
+    rich_black = rgb(0, 0, 0, maxColorValue = 255),
+    rich_black_90 = rgb(65, 64, 66, maxColorValue = 255),
+    rich_black_80 = "#342F2E",
+    rich_black_70 = "#58595b",
+    rich_black_60 = "#808285",
+    rich_black_50 = "#716C6B",
+    rich_black_40 = "#a7a9ac",
+    rich_black_30 = "#bcbec0",
+    rich_black_20 = "#BBB8B8",
+    rich_black_10 = "#D8D6D6"
+  )
+}
